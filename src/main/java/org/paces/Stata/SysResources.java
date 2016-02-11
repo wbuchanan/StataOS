e2ab2303b93aa46eb2f20cdd7e8bd37429932472
wbuchanan <william@williambuchanan.net>
2016-02-11 13:53:45 -0600

    Added ado wrapper and set up return values

--- src/main/java/org/paces/Stata/SysResources.java
+++ src/main/java/org/paces/Stata/SysResources.java
@@ -13,48 +13,32 @@ public class SysResources {
 	private static OperatingSystemMXBean osBean = ManagementFactory.getPlatformMXBean(OperatingSystemMXBean.class);
 
 	public static void main(String[] args) {
-		resources(args);
+		Resources r = new Resources(osBean);
+		System.out.println("Total committed memory is : " + r.prettyBytes(r.committedMemory));
+		System.out.println("Free Swap Space is : " + r.prettyBytes(r.freeSwap));
+		System.out.println("Total Available Swap Space is : " + r.prettyBytes(r.totalSwap));
+		System.out.println("Process CPU Time is : " + String.valueOf(r.processCPUTime));
+		System.out.println("Free Physical Memory Available is : " + r.prettyBytes(r.freePhysicalMem));
+		System.out.println("Total Physical Memory Available is : " + r.prettyBytes(r.totalPhysicalMem));
+		System.out.println("System CPU Load is : " + String.valueOf(r.systemCPULoad));
+		System.out.println("Process Load is : " + String.valueOf(r.processLoad));
+		System.out.println("% Free Physical Memory is : " + String.valueOf(r.pctFreeMemory));
 	}
 
-	public static int resources(String[] args) {
-		Long committedMemory = osBean.getCommittedVirtualMemorySize();
-		Long totalSwap = osBean.getTotalSwapSpaceSize();
-		Long freeSwap = osBean.getFreeSwapSpaceSize();
-		Long processCPUTime = osBean.getProcessCpuTime();
-		Long freePhysicalMem = osBean.getFreePhysicalMemorySize();
-		Long totalPhysicalMem = osBean.getTotalPhysicalMemorySize();
-		Double systemCPULoad = osBean.getSystemCpuLoad();
-		Double processLoad = osBean.getProcessCpuLoad();
-		System.out.println("Total committed memory is : " + prettyBytes(committedMemory));
-		System.out.println("Free Swap Space is : " + prettyBytes(freeSwap));
-		System.out.println("Total Available Swap Space is : " + prettyBytes(totalSwap));
-		System.out.println("Process CPU Time is : " + String.valueOf(processCPUTime));
-		System.out.println("Free Physical Memory Available is : " + prettyBytes(freePhysicalMem));
-		System.out.println("Total Physical Memory Available is : " + prettyBytes(totalPhysicalMem));
-		System.out.println("System CPU Load is : " + String.valueOf(systemCPULoad));
-		System.out.println("Process Load is : " + String.valueOf(processLoad));
-		System.out.println("% Free Physical Memory is : " + String.valueOf(freePhysicalMem / totalPhysicalMem));
-		return 0;
+
+	public static int compResources(String[] args) {
+		Resources sysresource = new Resources(osBean);
+		if (args.length == 1) {
+			sysresource.print();
+			sysresource.returnToStata();
+			return 0;
+		} else {
+			sysresource.returnToStata();
+			return 0;
+		}
 	}
 
-	public static String prettyBytes(long bytes) {
-		return humanReadableByteCount(bytes, false);
-	}
 
-	/**
-	 * Method copied from response on :
-	 * http://stackoverflow.com/questions/3758606/how-to-convert-byte-size-into-human-readable-format-in-java
-	 * From users BalusC and aioobe
-	 * @param bytes The byte value to convert
-	 * @param si The base value to use.  If false will assume 1024 bytes per MB
-	 * @return A formatted string containing the byte value
-	 */
-	private static String humanReadableByteCount(long bytes, boolean si) {
-		int unit = si ? 1000 : 1024;
-		if (bytes < unit) return bytes + " B";
-		int exp = (int) (Math.log(bytes) / Math.log(unit));
-		String pre = (si ? "kMGTPE" : "KMGTPE").charAt(exp-1) + (si ? "" : "i");
-		return String.format("%.1f %sB", bytes / Math.pow(unit, exp), pre);
-	}
+
 
 }
